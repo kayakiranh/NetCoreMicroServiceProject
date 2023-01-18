ac11e46a74cddeddf98f4af202cb50971e9819cb
Hüseyin <huseyin.kayakiran@gmail.com>
2023-01-19 00:12:23 +0300

    angular developments

--- MicroServiceProject/Source/UserInterface/MP.UserInterface.AngularUI/microserviceangularapp/src/app/app.module.ts
+++ MicroServiceProject/Source/UserInterface/MP.UserInterface.AngularUI/microserviceangularapp/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddcustomerComponent } from './addcustomer/addcustomer.component';
+import { UpdatecustomerComponent } from './components/updatecustomer/updatecustomer.component';
+import { RemovecustomerComponent } from './components/removecustomer/removecustomer.component';
+import { ListcustomerComponent } from './components/listcustomer/listcustomer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddcustomerComponent,
+    UpdatecustomerComponent,
+    RemovecustomerComponent,
+    ListcustomerComponent
   ],
   imports: [
     BrowserModule,
